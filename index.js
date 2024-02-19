80f97fb6f387a4837aa665e4d0df45c2abdc98a8
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2024-02-19 16:59:27 +0530

    🐛 Fix : cors issue

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import moongoose, { connect } from 'mongoose';
 import Express from 'express';
 import movie from "./routes/MovieRoutes.js";
 import { connectToDB } from './config/database.js';
+import cors from 'cors';
 
 const app = Express();
+app.use(cors({ origin: 'http://localhost:4200' }));
 app.use('/', movie);
 connectToDB();
+
 
 app.listen(
     3000,
